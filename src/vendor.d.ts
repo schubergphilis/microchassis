e4f50b5cc0c0cf933be80f333fbd61a53e936219
Andrii Mishkovskyi <mishok13@gmail.com>
2018-08-28 16:02:54 +0200

    Removes type declarations for gRPC
    Type declarations are now provided by grpc library itself.

--- src/vendor.d.ts
+++ src/vendor.d.ts
@@ -1,40 +1,40 @@
-// Partial implementation taken from
-// https://raw.githubusercontent.com/grpc/grpc-node/master/packages/grpc-native-core/index.d.ts
-// Further implementations should upgrade to latest gRPC version.
+// // Partial implementation taken from
+// // https://raw.githubusercontent.com/grpc/grpc-node/master/packages/grpc-native-core/index.d.ts
+// // Further implementations should upgrade to latest gRPC version.
 
-declare module 'grpc' {
-  export function load(filename: string): any;
-  export class Metadata {
-    add(key: string, value: string): void;
-  }
-  export class Server { }
-  export class ServerCredentials {
-    static createInsecure(): ServerCredentials;
-  }
-  export const credentials: {
-    createInsecure(): any;
-  };
+// declare module 'grpc' {
+//   export function load(filename: string): any;
+//   export class Metadata {
+//     add(key: string, value: string): void;
+//   }
+//   export class Server { }
+//   export class ServerCredentials {
+//     static createInsecure(): ServerCredentials;
+//   }
+//   export const credentials: {
+//     createInsecure(): any;
+//   };
 
-  export enum Status {
-    OK = 0,
-    CANCELLED,
-    UNKNOWN,
-    INVALID_ARGUMENT,
-    DEADLINE_EXCEEDED,
-    NOT_FOUND,
-    ALREADY_EXISTS,
-    PERMISSION_DENIED,
-    RESOURCE_EXHAUSTED,
-    FAILED_PRECONDITION,
-    ABORTED,
-    OUT_OF_RANGE,
-    UNIMPLEMENTED,
-    INTERNAL,
-    UNAVAILABLE,
-    DATA_LOSS,
-    UNAUTHENTICATED
-  }
-}
+//   export enum Status {
+//     OK = 0,
+//     CANCELLED,
+//     UNKNOWN,
+//     INVALID_ARGUMENT,
+//     DEADLINE_EXCEEDED,
+//     NOT_FOUND,
+//     ALREADY_EXISTS,
+//     PERMISSION_DENIED,
+//     RESOURCE_EXHAUSTED,
+//     FAILED_PRECONDITION,
+//     ABORTED,
+//     OUT_OF_RANGE,
+//     UNIMPLEMENTED,
+//     INTERNAL,
+//     UNAVAILABLE,
+//     DATA_LOSS,
+//     UNAUTHENTICATED
+//   }
+// }
 
 declare module 'grpc/src/grpc_extension' {
   export enum connectivityState {
